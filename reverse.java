a895b9182e861e3067f173aa0c28cd569a7902eb
Saravanan K <ksaravanansr7@gmail.com>
2025-07-05 21:45:22 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class reverse {
+ 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int rem,rev=0;
+      while(a!=0)
+      {
+          rem=a%10;
+          rev=rev*10+rem;
+          a=a/10;
+      }
+      System.out.print(rev+" ");
+    }
+}
+

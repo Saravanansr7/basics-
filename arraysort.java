a684d6234a0e626257eb4c5ab04be0f6416b5f4d
Saravanan K <ksaravanansr7@gmail.com>
2025-07-08 21:51:01 +0530

    Add files via upload

---
+++ arraysort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class arraysort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++){
+         arr[i]=sc.nextInt();
+            }int temp=0;
+          for(int i=0;i<a;i++){
+              for(int j=0;j<a;j++){
+                  if(arr[i]<arr[j]){
+                      temp=arr[i];
+                      arr[i]=arr[j];
+                      arr[j]=temp;
+                  }
+              
+          }
+          } 
+          for(int i=0;i<a;i++){
+        System.out.print(arr[i]+" ");
+            }
+          }
+    }
+    
+
